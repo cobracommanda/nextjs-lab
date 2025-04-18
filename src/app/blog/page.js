71703109dcb606b6817dba043742cd0dc35971dd
DRobinson <drobinson@benchmarkeducation.com>
2025-04-18 18:11:51 -0400

    updata prod domain fetching

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,18 +1,21 @@
-async function getData() {
-  // const endpoint = "http://localhost:3000/api/post";
-  // const res = await fetch(endpoint);
+import getDomain from "../lib/getDomain";
 
-  // if (!res) {
-  //   throw new Error("Failed to fetch data");
-  // }
-  // return res.json();
-  return { items: [] };
+async function getData() {
+  const domain = getDomain();
+  const endpoint = `${domain}/api/post`;
+  const res = await fetch(endpoint);
+
+  if (!res) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+  // return { items: [] };
 }
 
 export default async function BlogPage() {
   const data = await getData();
   const items = data && data.items ? [...data.items] : [];
-
+  console.log(process.env.PUBLIC_DOMAIN);
   return (
     <main>
       <h1>Jungle Juice</h1>
